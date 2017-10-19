5a2dfa16f4c8b702dd0642b31b72d0350fc1fa9f
tony <1619673196@qq.com>
2017-10-19 22:11:29 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/a16196/coolweather/gson/Basic.java
@@ -0,0 +1,9 @@
+package com.example.a16196.coolweather.gson;
+
+/**
+ * Created by 16196 on 2017/10/19.
+ */
+
+public class Basic {
+    
+}
--- app/src/main/java/com/example/a16196/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/example/a16196/coolweather/util/HttpUtil.java
@@ -1,8 +1,16 @@
 package com.example.a16196.coolweather.util;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by 16196 on 2017/10/19.
  */
 
 public class HttpUtil {
+    public static void  sendOKHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 }
